55ed794d2e3191fb2f795a46fc6225d250faee72
Samuel Shumake <shumakesam@gmail.com>
2022-03-17 12:12:21 -0400

    prevent infinite event listeners being passed upon continuous loop commands

--- commands/loop.js
+++ commands/loop.js
@@ -17,19 +17,23 @@ module.exports = {
         var resource;
 
         if (play.player && play.video && play.player['_state']['status'] == 'playing') {
-            return play.player.on('idle', () => {
+            // If user has already submitted the loop command for this song
+            if (play.player['_events']['idle']) return message.reply('> This song is already looping.');
+
+            play.player.on('idle', () => {
                 stream = ytdl(play.video, {filter: 'audioonly'});
                 resource = createAudioResource(stream);
                 play.player.play(resource);
-                // TODO: Test rewriting above block to just be a command.execute like in index.js
             });
-        }
+        } 
         if (playlist.player && playlist.video && playlist.player['_state']['status'] == 'playing') {
-            return playlist.player.on('idle', () => {
+            // If user has already submitted the loop command for this song
+            if (playlist.player['_events']['idle']) return message.reply('> This song is already looping.');
+            
+            playlist.player.on('idle', function loop() {
                 stream = ytdl(playlist.video, {filter: 'audioonly'});
                 resource = createAudioResource(stream);
                 playlist.player.play(resource);
-                // TODO: Test rewriting above block to just be a command.execute like in index.js
             });
         }
     }
